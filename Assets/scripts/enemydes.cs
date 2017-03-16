237ddc40563a1102f5126b2b99596717831468c8
aniket sharma <aniket965.as@gmail.com>
2017-03-16 20:27:55 +0530

    added pause button

---
+++ Assets/scripts/enemydes.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemydes : MonoBehaviour {
+
+		void Start () {
+	
+	}
+	
+	
+	void Update () {
+	
+	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "enemy") 
+        Destroy(coll.gameObject);
+    }
+    
+}
---
+++ Assets/scripts/uimanager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class uimanager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void Pause()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
