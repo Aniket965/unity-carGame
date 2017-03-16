90c5bbf8232258e572d2d74d9f70387bfed2a0dc
aniket sharma <aniket965.as@gmail.com>
2017-03-16 18:59:55 +0530

    enemy timer

--- Assets/scripts/carSpawnner.cs
+++ Assets/scripts/carSpawnner.cs
@@ -4,14 +4,23 @@ using System.Collections;
 public class carSpawnner : MonoBehaviour {
     public GameObject Car;
     public float maxPos = 1.9f;
+    public float delayedtimer = 1f;
+    float timer;
 
 	void Start () {
-        Vector3 carpos = new Vector3(Random.Range(-maxPos, maxPos), transform.position.y, transform.position.z);
-        Instantiate(Car,carpos,transform.rotation);
+          timer = delayedtimer;
+
 	}
 	
 
 	void Update () {
-	
-	}
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            Vector3 carpos = new Vector3(Random.Range(-maxPos, maxPos), transform.position.y, transform.position.z);
+            Instantiate(Car, carpos, transform.rotation);
+            timer = delayedtimer;
+        }
+       
+    }
 }
