ba4a4a3284663242a5cd6ed8adc1f5cb269b64f4
aniket sharma <aniket965.as@gmail.com>
2017-03-16 21:10:54 +0530

    added menu and score

--- Assets/scripts/uimanager.cs
+++ Assets/scripts/uimanager.cs
@@ -1,18 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class uimanager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    // Use this for initialization
+    public Text sText;
+    int score;
+    void Start () {
+        score = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Time.timeScale == 1)
+        {
+            score += 1;
+            sText.text = "Score: " + score;
+        }
 	}
-    void Pause()
+    public void Play()
+    {
+
+        Application.LoadLevel("level1");
+    }
+   public void Pause()
     {
         if (Time.timeScale == 1)
         {
