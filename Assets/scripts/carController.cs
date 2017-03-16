d44ea8cef2854c19db855f7d59f4291e7f572e4a
aniket sharma <aniket965.as@gmail.com>
2017-03-16 17:30:53 +0530

    1

---
+++ Assets/scripts/carController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carController : MonoBehaviour {
+
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/trackMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trackMove : MonoBehaviour {
+    public float speed;
+    Vector2 offset;
+
+	
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+        offset = new Vector2(0, speed * Time.time);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+
+	}
+}
